ef473847cc924b2219c6f4794aa1fff003e7bb6f
charukiewicz <charukiewicz@protonmail.com>
2020-11-22 19:23:09 -0600

    added config-desktop.nix and home-desktop.nix to nix/

---
+++ nix/home-desktop.nix
@@ -0,0 +1,45 @@
+{ config, pkgs, ... }:
+let
+  nixpkgs-unstable = import <nixpkgs-unstable> {};
+in
+{
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "christian";
+  home.homeDirectory = "/home/christian";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "20.09";
+
+
+  home.packages = with pkgs; [
+    jq
+    nq
+    yarn
+    yaml2json
+
+    nixpkgs-unstable.nix-prefetch-git
+
+
+    nixpkgs-unstable.haskellPackages.cabal-install
+    nixpkgs-unstable.haskellPackages.cabal2nix
+    nixpkgs-unstable.haskellPackages.ghcid
+
+    nixpkgs-unstable.elmPackages.elm
+
+    nixpkgs-unstable.hledger
+    nixpkgs-unstable.hledger-ui
+    nixpkgs-unstable.hledger-web
+  ];
+}
